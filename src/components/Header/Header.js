1adf876f1e40fb2cdfa70a4e1ab565d2cb8d4527
petya2323 <petyadeputat16@gmail.com>
2022-07-25 22:30:59 +0300

    feat: header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,15 @@
 import './Header.css'
 
 const Header = ({ }) => {
-    return (
-        <div className='header'>
-            Headersa
+    return ( 
+    <div className='header'>
+        <div className='inner_header'></div>
+        <div className='logo_container'>
+        <h1><img src='./logo.png' alt='hui'/></h1>
+        <div className='location'></div>
+        <h2><a>United Kingdom</a></h2>
         </div>
-    );
-}
-
-export default Header;
+    </div>
+    ) }
+    
+    export default Header
